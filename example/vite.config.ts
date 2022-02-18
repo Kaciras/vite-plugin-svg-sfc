41c531977363ce527cc56e65c6d9b445f5f7599a
Kaciras <Kaciras@protonmail.com>
2022-02-18 15:43:23 +0800

    fix `package.json`

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import svgSfc from "vite-svg-sfc";
+import svgSfc from "vite-plugin-svg-sfc";
 
 export default defineConfig({
 	base: "",
