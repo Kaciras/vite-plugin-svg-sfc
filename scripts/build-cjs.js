d9a910a7a8a3c7026a7586cc7b49e6f2d4d19ec9
Kaciras <Kaciras@protonmail.com>
2022-02-23 18:15:25 +0800

    add commonjs build

---
+++ scripts/build-cjs.js
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from "fs";
+import ts from "typescript";
+import tsConfig from "../tsconfig.json" assert { type: "json" };
+
+tsConfig.compilerOptions.module = "CommonJS";
+tsConfig.compilerOptions.declaration = false;
+
+const source = readFileSync("index.ts", "utf8");
+const output = ts.transpileModule(source, tsConfig);
+
+writeFileSync("index.cjs", output.outputText);
