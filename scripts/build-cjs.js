b015993bc45bd5de926f9212607b6ee6733b86d2
Kaciras <Kaciras@protonmail.com>
2023-10-05 14:59:31 +0800

    fix type errors

--- scripts/build-cjs.js
+++ scripts/build-cjs.js
@@ -3,8 +3,6 @@ import ts from "typescript";
 import tsConfig from "../tsconfig.json" assert { type: "json" };
 
 tsConfig.compilerOptions.module = "CommonJS";
-tsConfig.compilerOptions.declaration = false;
-tsConfig.compilerOptions.sourceMap = false;
 
 const source = readFileSync("index.ts", "utf8");
 const output = ts.transpileModule(source, tsConfig);
